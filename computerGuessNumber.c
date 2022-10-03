9ad4cb3a95980ad72913e2295c27e74850a2db75
Travis Logan <c1014427@newcastle.ac.uk>
2022-10-03 13:50:52 +0100

    Program to practice C and tackle coding problems

---
+++ computerGuessNumber.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/* 
+Write a program that picks a random number between 1 and 100 inclusive. 
+It then prompts the user to guess the number. If the user guesses correctly, 
+print out a message of congratulations and exit. Otherwise, tell the user whether their guess was low or high and ask them to guess again. 
+Repeat the process. If the user takes more than 7 guesses, tell them they need to think harder and exit. 
+You should use library functions srand and rand to seed and generate a random number. 
+The time function provides a good starting point for srand.
+*/
+
+
+
+int getRandNumber(){
+    // Get and return a number from the computer that is between 1 and 100.
+    srand(time(NULL));
+    int randomNum = rand() % 101;
+
+    // Redo if number is 0.
+    if (randomNum == 0){
+        getRandNumber();
+    }
+
+    return randomNum;
+}
+
+void guessNumber(int random){
+    int numGuess = 0;
+    int guessCount = 0;
+
+
+    while(1) {
+
+        printf("\nPlease enter a number that you would like to guess: ");
+        scanf("%d", &numGuess);
+
+        if (numGuess > random){
+            printf("This number is too high\n");
+            guessCount++;
+            
+        }
+        else if(numGuess < random){
+            printf("This number is too low\n");
+            guessCount++;
+
+        }
+        else if (numGuess == random){
+            printf("You have guessed!\n");
+            printf("It took you %d guesses", guessCount);
+            exit(0);
+        }
+        else {
+            printf("You have not inputted a valid input.");
+        }
+    }
+
+}
+
+int main(){
+
+    guessNumber(getRandNumber());
+
+    return 0;
+}
