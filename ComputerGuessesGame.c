91844aded5e525c34038223d05a5937bb73294d8
Travis Logan <c1014427@newcastle.ac.uk>
2022-10-03 15:10:08 +0100

    Program to have the computer guess the users number + boilerplate code outline

---
+++ ComputerGuessesGame.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+Ask the user to select a number. Then the program (computer) should guess and ask the user whether it's guess is correct, higher or lower. 
+The user enters an appropriate response. It is up to you to decide how the user indicates correct, higher or lower (hint: string comparison is best avoided). 
+Your program should implement a sensible guessing strategy. Try to implement the optimal strategy.
+*/
+
+int getNumber(){
+
+    return 0;
+}
+
+int generateGuess(){
+
+    return 0;
+}
+
+int main(){
+
+    return 0;
+}
