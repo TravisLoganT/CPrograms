1fcb32a74d1b1c09de78522a963c8dd9f2193f06
Travis Logan <c1014427@newcastle.ac.uk>
2022-10-03 16:34:18 +0100

    Work in progress, basic template to add upon

--- ComputerGuessesGame.c
+++ ComputerGuessesGame.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int HIGHER = 100;
+int LOWER = 1;
+int RANGE = 100 - 1;
+int USERNUM;
+int CORRECT = 0;
 
 /*
 Ask the user to select a number. Then the program (computer) should guess and ask the user whether it's guess is correct, higher or lower. 
@@ -7,16 +16,59 @@ Your program should implement a sensible guessing strategy. Try to implement the
 */
 
 int getNumber(){
+    printf("Please enter the number you would like to use: ");
+    scanf("%d", &USERNUM);
 
-    return 0;
+    return USERNUM;
 }
 
 int generateGuess(){
+    RANGE = HIGHER - LOWER;
 
-    return 0;
+    srand(time(NULL));
+    int guessedNumber = rand() % (RANGE + 1) + LOWER;
+
+    return guessedNumber;
+}
+
+int isCorrect(compNum){
+    char highLow[10];
+
+
+    if(USERNUM == compNum){
+        printf("The computer has guessed your Number!");
+        return 1;
+    }
+    else{
+        printf("Is %d higher or lower then your number? ", compNum);
+        scanf("%s", highLow);
+
+        if (strcmp(highLow, "Higher") != 0 || strcmp(highLow, "higher") != 0){
+            LOWER = compNum;
+            return CORRECT;            }
+        else if (strcmp(highLow, "Lower") != 0 || strcmp(highLow, "lower") != 0){
+            HIGHER = compNum;
+            return CORRECT;
+        }
+        else{
+            printf("You have not inpuuted a valid comparison");
+        }
+    }
+}
+
+
+
+
+void play(){
+    while(1){   
+        int computerGuess = generateGuess();
+        isCorrect(computerGuess);
+    }
 }
 
 int main(){
+    getNumber();
+    play();
 
     return 0;
 }
